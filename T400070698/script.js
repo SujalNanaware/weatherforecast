78af67b6e42af3625b54cffdb6452a4167172bba
SujalNanaware <sujal30nanaware@gmail.com>
2026-05-09 11:44:07 +0530

    Add files via upload

---
+++ T400070698/script.js
@@ -0,0 +1,36 @@
+async function getWeather() {
+
+    const city = document.getElementById("city").value;
+
+    const apiKey = "YOUR_API_KEY";
+
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        if (data.cod === "404") {
+            document.getElementById("weatherResult").innerHTML =
+            "City not found!";
+            return;
+        }
+
+        document.getElementById("weatherResult").innerHTML =
+        `
+        <h3>${data.name}</h3>
+        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+        `;
+
+    } catch (error) {
+
+        document.getElementById("weatherResult").innerHTML =
+        "Error fetching weather data!";
+    }
+}
